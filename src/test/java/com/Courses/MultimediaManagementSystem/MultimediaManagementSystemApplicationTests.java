128a54ab03a637e39adad64e066c2f9ad7d3fd61
JhonatanTH7 <Jhonatantorohurtado93@gmail.com>
2024-07-23 08:06:33 -0500

    initial commit

---
+++ src/test/java/com/Courses/MultimediaManagementSystem/MultimediaManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Courses.MultimediaManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimediaManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
