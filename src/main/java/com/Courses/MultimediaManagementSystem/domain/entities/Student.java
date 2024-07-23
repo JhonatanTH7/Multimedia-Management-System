088f36eebeb0e9eab25b74819aac46de13098a78
JhonatanTH7 <Jhonatantorohurtado93@gmail.com>
2024-07-23 10:26:35 -0500

    Entities

--- src/main/java/com/Courses/MultimediaManagementSystem/domain/entities/Student.java
+++ src/main/java/com/Courses/MultimediaManagementSystem/domain/entities/Student.java
@@ -1,5 +1,42 @@
 package com.Courses.MultimediaManagementSystem.domain.entities;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "student")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    private Boolean isActive;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_id_class", referencedColumnName = "id")
+    private ClassEntity classEntity;
+
 }
