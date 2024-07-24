97a57b74f30a7b7a99cb1be2ea30e6283abe139c
JhonatanTH7 <jhonatantorohurtado93@gmail.com>
2024-07-23 21:13:53 -0500

    Request

--- src/main/java/com/Courses/MultimediaManagementSystem/api/dto/request/StudentRequest.java
+++ src/main/java/com/Courses/MultimediaManagementSystem/api/dto/request/StudentRequest.java
@@ -1,9 +1,9 @@
 package com.Courses.MultimediaManagementSystem.api.dto.request;
 
-import com.Courses.MultimediaManagementSystem.api.dto.request.update.StudentUpdateRequest;
-
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,7 +14,19 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class StudentRequest extends StudentUpdateRequest {
+public class StudentRequest {
+
+    @Schema(description = "Name of the Student", example = "John Doe")
+    @NotBlank(message = "The Student name is required")
+    private String name;
+
+    @Schema(description = "Email of the Student", example = "JohnDoe@Example.com")
+    @NotBlank(message = "The email of the Student is required")
+    @Email(message = "Enter a valid email address")
+    private String email;
+
+    @Schema(description = "Status of the Student, it could be either true or false", example = "true")
+    private Boolean isActive;
 
     @Schema(description = "Id of the Class", example = "1")
     @NotNull(message = "The id of the Class is required")
