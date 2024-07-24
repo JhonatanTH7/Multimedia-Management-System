519da5eedc974ed9087e4dc178078ba8eeaea155
JhonatanTH7 <Jhonatantorohurtado93@gmail.com>
2024-07-24 06:56:52 -0500

    Repositories

--- src/main/java/com/Courses/MultimediaManagementSystem/domain/repositories/ClassEntityRepository.java
+++ src/main/java/com/Courses/MultimediaManagementSystem/domain/repositories/ClassEntityRepository.java
@@ -1,9 +1,11 @@
 package com.Courses.MultimediaManagementSystem.domain.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.Courses.MultimediaManagementSystem.domain.entities.ClassEntity;
 
+@Repository
 public interface ClassEntityRepository extends JpaRepository<ClassEntity, Long> {
 
 }
--- src/main/java/com/Courses/MultimediaManagementSystem/domain/repositories/StudentRepository.java
+++ src/main/java/com/Courses/MultimediaManagementSystem/domain/repositories/StudentRepository.java
@@ -1,9 +1,11 @@
 package com.Courses.MultimediaManagementSystem.domain.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.Courses.MultimediaManagementSystem.domain.entities.Student;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
 }
