40e3df509f38a6142d90d36ddd134a3345a75767
JhonatanTH7 <Jhonatantorohurtado93@gmail.com>
2024-07-24 09:19:40 -0500

    Repositories

--- src/main/java/com/Courses/MultimediaManagementSystem/domain/repositories/StudentRepository.java
+++ src/main/java/com/Courses/MultimediaManagementSystem/domain/repositories/StudentRepository.java
@@ -1,11 +1,18 @@
 package com.Courses.MultimediaManagementSystem.domain.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.Courses.MultimediaManagementSystem.domain.entities.Student;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    @Query("SELECT s FROM student s WHERE s.name LIKE %:name% AND s.email LIKE %:email% AND s.isActive = true")
+    Page<Student> findAll(Pageable pageable, @Param("name") String name, @Param("email") String email);
 
 }
